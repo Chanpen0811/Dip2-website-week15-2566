f1d8fad5fc727f7c147046a037fb106e9be640a5
chanpen0811 <jan1574p@gmail.com>
2024-01-30 15:52:59 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: ()=>import('../views/HomeView.vue')
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/sign-up',
+      name: 'signup',
+      component: () => import('../views/SignUp.vue')
+    },
+    {
+      path: '/sign-in',
+      name: 'signin',
+      component: () => import('../views/SignIn.vue')
     },
     {
       path: '/about',
